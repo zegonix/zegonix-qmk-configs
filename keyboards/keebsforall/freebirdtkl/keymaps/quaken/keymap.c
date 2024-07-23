2944382645a367df7bdec4b87a51ace47f3e0449
scbj <scbj@zhaw.ch>
2024-07-23 17:03:20 +0200

    changed fbtkl keymap

--- keyboards/keebsforall/freebirdtkl/keymaps/quaken/keymap.c
+++ keyboards/keebsforall/freebirdtkl/keymaps/quaken/keymap.c
@@ -36,7 +36,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_tkl_f13_ansi(
     KC_ESC,    KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    KC_F13,    DF(_BASE), KC_SCRL,   DF(_GAME),
     KC_ESC,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,   KC_INS,    KC_HOME,   KC_PGUP,
-    KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_BSLS,   KC_DEL,    KC_END,    KC_PGDN,
+    KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_BSLS,   KC_BSLS,   KC_BSLS,   KC_DEL,    KC_END,    KC_PGDN,
     MO(_CHAR), QUAK_A,    QUAK_S,    QUAK_D,    QUAK_F,    KC_G,      KC_H,      QUAK_J,    QUAK_K,    QUAK_L,    QUAK__,    KC_QUOT,              KC_ENT,
     KC_LSFT,              KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,              KC_RSFT,              KC_UP,
     KC_LCTL,   KC_LGUI,   KC_LALT,                                    KC_SPC,                                     MO(_NAV),  KC_LCTL,   KC_RGUI,   KC_RALT,   KC_LEFT,   KC_DOWN,   KC_RGHT
@@ -66,10 +66,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [3] = LAYOUT_tkl_f13_ansi(
     _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   QK_BOOT,   _______,
     _______,   KC_F1,     KC_F2,     KC_F3,     KC_F4,     KC_F5,     KC_F6,     KC_F7,     KC_F8,     KC_F9,     KC_F10,    KC_F11,    KC_F12,    _______,   _______,   _______,   _______,
-    _______,   _______,   _______,   _______,   _______,   DF(_GAME), _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,
-    _______,   _______,   _______,   _______,   _______,   DF(_BASE), _______,   KC_DEL,    _______,   _______,   _______,   _______,              _______,
+    _______,   _______,   _______,   KC_VOLD,   KC_VOLU,   DF(_GAME), _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,
+    _______,   KC_LGUI,   KC_LALT,   KC_LSFT,   KC_LCTL,   DF(_BASE), KC_DEL,    KC_MINS,   KC_EQL,    KC_LBRC,   KC_RBRC,   _______,              _______,
     _______,              _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,              _______,              _______,
-    _______,   _______,   _______,                                    KC_CAPS,                                    _______,   _______,   _______,   _______,   _______,   _______,   _______
+    QK_BOOT,   _______,   _______,                                    KC_CAPS,                                    _______,   _______,   _______,   _______,   _______,   _______,   _______
     ),
 
     // layout template
