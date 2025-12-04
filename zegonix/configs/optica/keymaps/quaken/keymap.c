fedd6096f1dc8e99ae7f06ffdef95b6066a3365e
scbj <scbj@zhaw.ch>
2025-12-04 09:28:44 +0100

    updated keymap for the optica

--- zegonix/configs/optica/keymaps/quaken/keymap.c
+++ zegonix/configs/optica/keymaps/quaken/keymap.c
@@ -17,7 +17,6 @@ enum layer_names {
     _GAME,
     _NAV,
     _DIV,
-    _MOUS,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -50,8 +49,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 KC_ESC,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_MINS,   KC_EQL,    KC_BSPC,
 KC_TAB,    KC_Q,      KC_W,      KC_E,      KC_R,      KC_T,      KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_LBRC,   KC_RBRC,   KC_BSLS,
 MO(_DIV),  QUAK_A,    QUAK_S,    QUAK_D,    QUAK_F,    KC_G,      KC_H,      QUAK_J,    QUAK_K,    QUAK_L,    QUAK__,    KC_QUOT,              KC_ENT,
-KC_LSFT,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,              KC_RSFT,   KC_MPLY,
-KC_LCTL,   KC_LGUI,   KC_LALT,                         KC_SPC,                                                MO(_NAV),  KC_LCTL,   KC_LGUI,   KC_LALT
+KC_LSFT,   KC_Z,      KC_X,      KC_C,      KC_V,      KC_B,      KC_N,      KC_M,      KC_COMM,   KC_DOT,    KC_SLSH,              KC_RSFT,   KC_UP,
+KC_LCTL,   KC_LGUI,   KC_LALT,                         KC_SPC,                                                MO(_NAV),  KC_LCTL,   KC_LGUI,   KC_DOWN
 ),
 
 // gaming layer
@@ -79,15 +78,6 @@ _______,   _______,   _______,   KC_VOLD,   KC_VOLU,   DF(_GAME), _______,   ___
 _______,   _______,   KC_LALT,   KC_LSFT,   KC_LCTL,   DF(_BASE), KC_DEL,    KC_MINS,   KC_EQL,    KC_LBRC,   KC_RBRC,   KC_BSLS,              _______,
 _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,              _______,   _______,
 QK_BOOT,   _______,   _______,                         KC_CAPS,                                               _______,   _______,   _______,   _______
-),
-
-// mouse layer
-[_MOUS] = LAYOUT(
-_______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,
-_______,   _______,   _______,   _______,   _______,   _______,   KC_WH_U,   KC_BTN1,   KC_MS_U,   KC_BTN2,   _______,   _______,   _______,   _______,
-_______,   _______,   _______,   _______,   _______,   _______,   KC_WH_D,   KC_MS_L,   KC_MS_D,   KC_MS_R,   _______,   _______,              _______,
-_______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,   _______,              _______,   _______,
-_______,   _______,   _______,                         _______,                                               _______,   _______,   _______,   _______
 ),
 };
 
